aa5c1bd9e43d0b7483325e92b5deb515bb8232fa
Punam Surana <suranapunam07@gmail.com>
2026-09-25 17:18:00 +0530

    Allow Netlify frontend in CORS

--- job_tracker/app/main.py
+++ job_tracker/app/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:3000",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://punam-job-application-tracker.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
